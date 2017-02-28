f7fd1e0cc65131098e5a33b81da486412fcc652d
LizaDakhova <donskaya0598@gmail.com>
2017-02-28 17:47:16 +0300

    Упрощение

--- src/cpp/lesson4/main.cpp
+++ src/cpp/lesson4/main.cpp
@@ -2,11 +2,13 @@
 // Created by dmitry on 2/28/17.
 //
 
-#include "Matrix.h"
+//#include "Matrix.h"
+#include <iostream>
 
 int main() {
-    Matrix m(2, 2);
+    /*Matrix m(2, 2);
     std::cin >> m;
-    std::cout << m;
+    std::cout << m;*/
+	std::cout << "Hello, world!\n";
     return 0;
 }
