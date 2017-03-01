da617f89aec92b05a3161539f74af04fb9207e9f
LizaDakhova <donskaya0598@gmail.com>
2017-03-01 15:55:50 +0300

    Дописала оставшиеся функции

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -4,10 +4,19 @@
 int main() {
 	Matrix a(2, 2);
 	std::cin >> a;
-	std::cout << a;
+	std::cout << "a" << '\n' << a;
 	Matrix b(a);
-	std::cout << b;
-	std::cout << a + b;
+	std::cout << "b" << '\n' << b;
+	std::cout << "a + b" << '\n' << a + b;
+	int n = 6;
+	std::cout << "a + " << n << '\n' << a + n;
+	std::cout << "a * b" << '\n' << a * b;
+	int k = 2;
+	std::cout << "a * " << k << '\n' << a * k;
+	int count = 3;
+	std::cout << "eye(" << count << ")" << '\n' << Matrix::eye(count);
+	std::cout << "eye(2, 1 3)" << '\n' << Matrix::eye(2, 1, 3);
+	std::cout << "eye_conversely(" << count << ")" << '\n' << Matrix::eye_conversely(count);
 	system("pause");
 	return 0;
 }
